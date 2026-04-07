897a0f3307b11b139eb1b0dd4c96c42ab5b2235f
sinkecpt025 <sinkecpt025@student.wethinkcode.co.za>
2026-04-07 09:22:48 +0200

    update: resume link

--- app/data.ts
+++ app/data.ts
@@ -228,7 +228,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
   },
   {
     label: 'Resume',
-    link: 'https://drive.google.com/file/d/1ZrTX_1gdBde0oAfcXOEWculjLGnyfrxh/view?usp=sharing',
+    link: 'https://drive.google.com/file/d/1DCgVvkk0NCHL_lJSnmjjTAIK0VtUnkJC/view?usp=sharing',
   },
   {
     label: 'npm',
